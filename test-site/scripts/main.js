1edcda1f22d8e135fb6d89cef77cfd68e3eb806d
Adam Frumkin <79058920+ajfrumkin@users.noreply.github.com>
2023-01-30 02:42:05 +0000

    JavaScript Basics

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -2,9 +2,9 @@ const myImage = document.querySelector("img");
 
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
+  if (mySrc === "images/20221225_101650.jpg") {
     myImage.setAttribute("src", "images/firefox2.JPG");
   } else {
-    myImage.setAttribute("src", "images/firefox-icon.png");
+    myImage.setAttribute("src", "images/20221225_101650.jpg");
   }
 };
