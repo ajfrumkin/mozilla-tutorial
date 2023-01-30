90d60d5456cbb9f4ea8ba45bbf9729262fe27cad
Adam Frumkin <79058920+ajfrumkin@users.noreply.github.com>
2023-01-30 02:53:20 +0000

    JavaScript Basics

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -8,3 +8,25 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "images/bikefront.JPG");
   }
 };
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+
+  
+  myButton.onclick = () => {
+    setUserName();
+  };
+  
