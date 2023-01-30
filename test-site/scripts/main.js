8e86e6f64e768f4900c9e0a94b8be65410e9a123
Adam Frumkin <79058920+ajfrumkin@users.noreply.github.com>
2023-01-30 02:36:49 +0000

    JavaScript Basics

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
